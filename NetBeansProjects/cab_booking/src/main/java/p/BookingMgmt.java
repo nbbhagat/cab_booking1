f8695da76fe123e80e7df7c837590b3b57a892dc
Pranav Menon <pranav.menon@oracle.com>
2020-11-06 11:33:02 +0530

    Adding base classes

---
+++ NetBeansProjects/cab_booking/src/main/java/p/BookingMgmt.java
@@ -0,0 +1,21 @@
+package p;
+public class BookingMgmt {
+	String passengerID;
+	String driverID;
+	String bookingID;
+	public void findNearestCab() {
+		
+	}
+	public void calculatePredictedFare() {
+		
+	}
+	public void driverRequest() {
+		
+	}
+	public void generateOTP() {
+		
+	}
+	public void createRide() {
+		
+	}
+}
---
+++ NetBeansProjects/cab_booking/src/main/java/p/Ride.java
@@ -0,0 +1,16 @@
+package p;
+public class Ride {
+	long startTime;
+	long endTime;
+	float rating;
+	String status;
+	Location source;
+	Location destination;
+	String date;
+	long otp;
+	String bookingID;
+	long waitingTime;
+	public void calculatePredictedFare() {
+		
+	}
+}
