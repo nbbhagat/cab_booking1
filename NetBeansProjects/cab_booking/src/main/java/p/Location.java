5cbed1daf1edcb6c57b7a1252079a0f3574bcacd
nbbhagat <35588311+nbbhagat@users.noreply.github.com>
2020-11-06 14:37:47 +0530

    Update Location.java

--- NetBeansProjects/cab_booking/src/main/java/p/Location.java
+++ NetBeansProjects/cab_booking/src/main/java/p/Location.java
@@ -1,5 +1,9 @@
 package p;
 public class Location {
-    int longitude;
     int latitude;
+    int longitude;
+    public Location(int latitude, int longitude){
+        this.latitude=latitude;
+        this.longitude=longitude;
+    }
 }
