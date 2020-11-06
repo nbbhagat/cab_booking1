a4689a85ba04ea9c4fae14e3aee5d8948bb08371
Pranav Menon <pranav.menon@oracle.com>
2020-11-06 15:20:34 +0530

    Adding functions to BookingMgmt

--- NetBeansProjects/cab_booking/src/main/java/p/Ride.java
+++ NetBeansProjects/cab_booking/src/main/java/p/Ride.java
@@ -3,13 +3,16 @@ public class Ride {
 	long startTime;
 	long endTime;
 	float rating;
-	String status;
+	String status; //started, completed, waiting(?)
 	Location source;
-	Location destination;
+	Location dest;
 	String date;
 	long otp;
 	String bookingID;
 	long waitingTime;
+	public Ride() {
+		
+	}
 	public void calculatePredictedFare() {
 		
 	}
