da0070b69b1495f3fc1877239d51bc7ed05a1273
Pranav Menon <pranav.menon@oracle.com>
2020-11-11 02:05:14 +0530

    Removed calculateActualFare() from Ride

--- NetBeansProjects/cab_booking/src 2.23.19 PM/main/java/p/Ride.java
+++ NetBeansProjects/cab_booking/src 2.23.19 PM/main/java/p/Ride.java
@@ -1,19 +1,30 @@
 package p;
-public class Ride {
+public class Ride implements java.io.Serializable{
 	long startTime;
 	long endTime;
-	float rating;
+	int rating;
 	String status; //started, completed, waiting(?)
 	Location source;
 	Location dest;
 	String date;
-	long otp;
+//	long otp;
 	String bookingID;
 	long waitingTime;
-	public Ride() {
-		
+	public Ride(long startTime, Location source, Location dest, String date, String bookingID) {
+		this.startTime = startTime;
+		this.source = source;
+		this.dest = dest;
+		this.date = date;
+		this.bookingID = bookingID;
+		this.status = "Started";
 	}
-	public void calculatePredictedFare() {
-		
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
 	}
 }
