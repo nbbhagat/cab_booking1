0019f8ecc92b8b1a49eb620c4b1285fd6f48b836
namrata <nbhagatiitbhu@gmail.com>
2020-11-10 14:30:08 +0530

    merging again

---
+++ NetBeansProjects/cab_booking/src 2.23.19 PM/main/java/App.java
@@ -0,0 +1,10 @@
+import java.util.*; 
+import java.io.*;
+public class App {
+
+    public static void main(String [] args) {
+
+       
+
+    }
+}
---
+++ NetBeansProjects/cab_booking/src 2.23.19 PM/main/java/p/Ride.java
@@ -0,0 +1,19 @@
+package p;
+public class Ride {
+	long startTime;
+	long endTime;
+	float rating;
+	String status; //started, completed, waiting(?)
+	Location source;
+	Location dest;
+	String date;
+	long otp;
+	String bookingID;
+	long waitingTime;
+	public Ride() {
+		
+	}
+	public void calculatePredictedFare() {
+		
+	}
+}
