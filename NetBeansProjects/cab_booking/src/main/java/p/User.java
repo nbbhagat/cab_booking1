68b34343405d0b9c567c1885d765f59412f0adcc
nbhagatiitbhu@gmail.com <nbhagatiitbhu@gmail.com>
2020-11-11 17:37:30 +0530

    updated user

--- NetBeansProjects/cab_booking/src/main/java/p/User.java
+++ NetBeansProjects/cab_booking/src/main/java/p/User.java
@@ -1,6 +1,8 @@
 package p;
+import java.util.UUID;
+import java.util.ArrayList;
 
-public class User {
+public class User implements java.io.Serializable {
     String userId;
     String name;
     String phoneNo;
@@ -8,12 +10,17 @@ public class User {
     String password;
     Location location;
     float avgRating=0;
-    public User(String name, String phoneNo,String password,Location location){
+    String region;
+    public User(){
+
+    }
+    public User(String name, String phoneNo,String password,Location location,String region){
         this.userId=userId;
         this.name=name;
         this.phoneNo=phoneNo;
         this.password=password;
         this.location=location;
+        this.region=region;
     }
     public void setNumRides() {
         this.numRides++;
@@ -27,21 +34,69 @@ public class User {
     public void setAvgRating(int rating) {
         this.avgRating=avgRating+(rating-avgRating)/(numRides);
     }
-    public  void viewRideHistory(){
-        
+    public Location getLocation() {
+        return location;
+    }
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+        public  void viewRideHistory(MemManager mmap,String userId){
+        ArrayList<String> record= mmap.userBooking.get(userId);
+            if(record!=null)
+            {
+                for (int i = 0; i < record.size(); i++) {
+                        String bookingId=record.get(i) ;
+                        Ride ride=mmap.rideMap.get(i);
+                        System.out.println(i+1+". "+ride);
+                }
+
+            }
+            else  System.out.println("No record found");
+    }
+
+    public  void viewPaymentHistory(MemManager mmap,String userId){
+            ArrayList<String> record= mmap.userBooking.get(userId);
+            if(record!=null)
+            {
+                for (int i = 0; i < record.size(); i++) {
+                        String bookingId=record.get(i) ;
+                        Payment payment =mmap.payMap.get(i);
+                        System.out.println(i+1+". "+payment);
+                }
+
+            }
+            else  System.out.println("No record found");
     }
 }
 class Passenger extends User{
     Address address;
-    public Passenger(String name, String phoneNo,String password,Location location){
-        super(name,phoneNo,password,location);
+    public Passenger(String name, String phoneNo,String password,Location location,String region){
+        super(name,phoneNo,password,location,region);
         this.address=address;
+    }
+    @Override
+    public String toString(){
+        return "name:-> "+name+", phoneNo-> "+phoneNo+", avgRating-> "+avgRating+", location-> "+location+" "+address;
     }
 }
 class Driver extends User{
-    boolean status;
-    public Driver(String name, String phoneNo,String password,Location location, boolean status,Vehicle vehicle){
-        super(name,phoneNo,password,location);
-        this.status=status;
-    }
+        boolean status;
+        public Driver(String name, String phoneNo,String password,Location location, boolean status,Vehicle vehicle,String region){
+                super(name,phoneNo,password,location,region);
+                this.status=status;
+                this.userId="d"+UUID.randomUUID().toString();
+        }
+
+        public boolean isStatus() {
+            return status;
+        }
+
+        public void setStatus() {
+            this.status = !status;
+        }
+
+        @Override
+        public String toString(){
+            return "name:-> "+name+", phoneNo-> "+phoneNo+", avgRating-> "+avgRating+", location-> "+location;
+        }
 }
