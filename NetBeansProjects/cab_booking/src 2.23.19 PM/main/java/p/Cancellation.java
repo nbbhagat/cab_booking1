640768112014a201a2497be2473f35b946ff47ea
Pranav Menon <pranav.menon@oracle.com>
2020-11-11 11:29:33 +0530

    Added constructor to Cancellation class

--- NetBeansProjects/cab_booking/src 2.23.19 PM/main/java/p/Cancellation.java
+++ NetBeansProjects/cab_booking/src 2.23.19 PM/main/java/p/Cancellation.java
@@ -3,5 +3,10 @@ public class Cancellation {
 	String bookingID;
 	String reason;
 	float penaltyAmt;
-	String cancelledBy;
+//	String cancelledBy;
+	public Cancellation(String bookinID, String reason, float penaltyAmt) {
+		this.bookingID = bookingID;
+		this.reason = reason;
+		this.penaltyAmt = penaltyAmt;
+	}
 }
